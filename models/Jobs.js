4c939845a921eacbfb126d3d9450cd6c096983a3
ethanvannest <ethan3vannest@gmail.com>
2022-09-22 23:50:19 -0600

    jobs.js schema complete

--- models/Jobs.js
+++ models/Jobs.js
@@ -1,7 +1,28 @@
 const mongoose = require('mongoose');
 
 const jobsSchema = new mongoose.Schema({
-
+company: {
+    type: String, 
+    required: true
+},
+job: {
+    type: String, 
+    required: true
+},
+salary: {
+    type: Number, 
+    default: 0
+}, 
+date: {
+    type: String, 
+    required: true
+},
+offer: {
+    type: Boolean, 
+    default: false
+}, 
+Notes: [{ type: String }] 
+    
 })
 
 /* our schecma -> complied -> model -> is the one that have access to all the methods */
