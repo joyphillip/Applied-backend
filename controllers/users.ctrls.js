756af5271972fa926173269a7dbf0b852efa83ff
ethanvannest <ethan3vannest@gmail.com>
2022-09-24 08:40:37 -0600

    post route sign up

--- controllers/users.ctrls.js
+++ controllers/users.ctrls.js
@@ -1,6 +1,20 @@
-
+const db = require('../models')
+const bcrypt = require('bcrypt')
 // POST ROUTE sign up
+const signup = (req, res) => {
+  req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10))
 
+  db.Users.create(req.body, (error, createdUser) => {
+    if(error){
+      res.status(400).json({
+        error: error.message
+      })
+    } else {
+      console.log('user has been registered')
+      res.status(201).json(createdUser)
+    }
+  })
+}
 
 // USER LOGIN ROUTE (CREATE SESSION)
 
