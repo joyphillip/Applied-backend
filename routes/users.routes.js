1060ca9eddc90d2a12e8d7e292d8ac59ad6da8cd
JPhillip018 <jphillip018@gmail.com>
2022-09-28 21:29:36 -0400

    updated user route

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 const ctrls = require('../controllers')
 
-router.get('/', ctrls.users)
+router.get('/', ctrls.users.signup)
 router.post('/signup', ctrls.users.signup)
 router.post('/login', ctrls.users.login)
 router.delete('/logout', ctrls.users.logout)
