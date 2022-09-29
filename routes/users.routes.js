984eb17a78369b1bc6ab562749f1f5ee3852b8a7
JPhillip018 <jphillip018@gmail.com>
2022-09-28 21:27:01 -0400

    added get route to users

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 const ctrls = require('../controllers')
 
+router.get('/', ctrls.users)
 router.post('/signup', ctrls.users.signup)
 router.post('/login', ctrls.users.login)
 router.delete('/logout', ctrls.users.logout)
